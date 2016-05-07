a409ad1bc8a2be68457ba4b6f4342eddb2824b65
MohamedA-M-Hassan <anehmr0@gmail.com>
2016-05-07 23:23:05 +0200

    best fit

--- allocation.cpp
+++ allocation.cpp
@@ -41,3 +41,48 @@ void Allocation::FirstFit(Memory &m ,Entity x)
 //{
 
 //}
+
+void Allocation::BestFit(Memory &m,Entity x)
+{
+
+    QList<Entity>::iterator it= m.mem.begin();
+    int min_size = -1;
+    int address_of_min_size;
+    if (x.get_size()>= it->get_size())
+    {
+         min_size=it->get_size();
+         address_of_min_size = it->get_base_address() ;
+    }
+    for(;it != m.mem.end();++it)
+    {
+        if((it->get_size() < min_size) && (x.get_size() >= it->get_size()))
+        {
+            min_size = it->get_size();
+            address_of_min_size = it->get_base_address() ;
+        }
+    }
+    if (min_size == -1)//kda yb2a mfysh mkan fy el mem kolha as8r mn size el process
+    {
+        return;
+    }
+    //prefect case
+    else if (x.get_size()==min_size)
+    {
+        it->set_type(true);
+        it->set_name(x.get_name());
+    }
+    else
+    {
+
+        // create new hole
+        Entity temp(false);
+        temp.set_size(it->get_size() - x.get_size());
+
+        m.mem.push_back(temp);
+
+        // allocate
+        it->set_type(true);
+        it->set_name(x.get_name());
+        it->set_size(x.get_size());
+    }
+}
